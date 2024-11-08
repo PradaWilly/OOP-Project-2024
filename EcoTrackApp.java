ebcb5d21c55326ec7da0db18145440f090d8e193
PradaWilly <112588780+PradaWilly@users.noreply.github.com>
2024-11-08 15:30:43 +0200

    Add files via upload

---
+++ EcoTrackApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ecotrackapp;
+
+/**
+ *
+ * @author cosmy
+ */
+public class EcoTrackApp {
+
+
+    public static void main(String[] args) {
+        
+        LoginGUI Login = new LoginGUI();
+        Login.setVisible(true);
+        
+    }
+    
+}
