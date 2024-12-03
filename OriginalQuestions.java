e526e2c467e3575ac7b063671ddfb7fcf34eab9a
rossp2005 <148553377+rossp2005@users.noreply.github.com>
2024-12-03 15:15:29 +0000

    Ross-updated files

---
+++ OriginalQuestions.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ecotrackapp;
+
+/**
+ *
+ * @author Ross
+ */
+public class OriginalQuestions {
+   public String question4;
+    public String question5;
+    public String question6;
+
+     public OriginalQuestions() {
+        this.question4 = "how often do you use your washing mashine?";
+        this.question5 = "what appliance do you use most at home? ";
+        this.question6 = "do you switch off your appliances when you dont use them?";
+    }
+
+    public String getQuestion4() {
+        return question4;
+    }
+
+    public void setQuestion4(String question4) {
+        this.question4 = question4;
+    }
+
+    public String getQuestion5() {
+        return question5;
+    }
+
+    public void setQuestion5(String question5) {
+        this.question5 = question5;
+    }
+
+    public String getQuestion6() {
+        return question6;
+    }
+
+    public void setQuestion6(String question6) {
+        this.question6 = question6;
+    }
+}
